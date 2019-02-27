985d624f1ff9ce3ced350c6c55962a8ae033f602
a_tanaka <a_tanaka@timedia.co.jp>
2019-02-27 13:20:56 +0900

    [add] 数式関係のノード定義を追加

--- src/MapGrammar/AstNodes/mapGrammarAstNodes.ts
+++ src/MapGrammar/AstNodes/mapGrammarAstNodes.ts
@@ -8,7 +8,29 @@ import { MapFunction } from "../mapFunction";
 export enum MapGrammarType {
   Root,
   Distance,
+  VarAssign,
+  Calc_Addition,
+  Calc_Subtraction,
+  Calc_Multiplication,
+  Calc_Division,
+  Calc_Modulo,
+  Calc_Unary,
+  Calc_Abs,
+  Calc_Atan2,
+  Calc_Ceil,
+  Calc_Cos,
+  Calc_Exp,
+  Calc_Floor,
+  Calc_Log,
+  Calc_Pow,
+  Calc_Rand,
+  Calc_Sin,
+  Calc_Sqrt,
   CurveSetgaugeNode,
+  Number,
+  DistanceVariable,
+  String,
+  Var,
 }
 
 /**
@@ -56,6 +78,14 @@ export abstract class SyntaxWithKeyNode extends SyntaxNode {
 }
 
 /**
+ * 二項演算数式のASTノードベースクラス。
+ */
+export abstract class InfixExpressionNode extends MapGrammarAstNode {
+  left: MapGrammarAstNode | null = null
+  right: MapGrammarAstNode | null = null
+}
+
+/**
  * ルートノード。
  * ASTの根となるノードで、範囲は全構文が含まれます。
  * 各構文はstatementsに格納されています。
@@ -74,6 +104,21 @@ export class DistanceNode extends MapGrammarAstNode {
   type: MapGrammarType = MapGrammarType.Distance
   value: MapGrammarAstNode | null = null
 }
+
+/**
+ * 変数宣言ノード。
+ */
+export class VarAssignNode extends MapGrammarAstNode {
+  type: MapGrammarType = MapGrammarType.VarAssign
+  varName: string | null = null
+  value: MapGrammarAstNode | null = null
+}
+
+//#region 数式関係
+export class AdditionNode extends InfixExpressionNode {
+
+}
+//#endregion
 
 /**
  * Curve.SetGauge(value)ノード。
