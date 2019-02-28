de9056caa2e5e02ef2c2683ef59eb687dc168e01
aoisupersix <aoisupersix@gmail.com>
2019-02-28 20:15:52 +0900

    [add] ErrorListenerを追加

--- src/MapGrammar/MapGrammarParser.ts
+++ src/MapGrammar/MapGrammarParser.ts
@@ -1,17 +1,21 @@
-import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts'
+import { ANTLRInputStream, CommonTokenStream, ANTLRErrorListener, Token } from 'antlr4ts'
 import { MapGrammarVisitor, AstNode } from './MapGrammarVisitor'
 import { MapGrammarLexer } from './Parser/MapGrammarLexer'
 import * as parser from './Parser/MapGrammarParser'
-import { RootNode, MapGrammarType } from './AstNodes/mapGrammarAstNodes';
 import { MapGrammarErrorStrategy } from './mapGrammarErrorStrategy';
+import { ParserErrorListener } from '../errorListener';
 
 export class MapGrammarParser {
+
+  errorListener: ANTLRErrorListener<Token> = new ParserErrorListener()
+
   parse(input: string) {
     let inputStream = new ANTLRInputStream(input)
     let lexer = new MapGrammarLexer(inputStream)
     let tokenStream = new CommonTokenStream(lexer)
     let antlrParser = new parser.MapGrammarParser(tokenStream)
     antlrParser.errorHandler = new MapGrammarErrorStrategy()
+    antlrParser.addErrorListener(this.errorListener)
     let tree = antlrParser.root()
   
     const mapGrammarVisitor = new MapGrammarVisitor()
