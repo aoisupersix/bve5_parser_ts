7dfed125e86ca0d0900a8ae37efb70c0d70e2a11
aoisupersix <aoisupersix@gmail.com>
2019-08-24 08:42:12 +0900

    [change] Move expression nodes to another file

--- src/map-grammar/ast-nodes/mapGrammarAstNodes.ts
+++ src/map-grammar/ast-nodes/mapGrammarAstNodes.ts
@@ -1,6 +1,7 @@
 import { Token } from "../../token";
 import { MapElement } from "../mapElement";
 import { MapFunction } from "../mapFunction";
+import { exprNode } from "./exprNode";
 
 /**
  * 全てのマップ構文ASTノードです。
@@ -8,14 +9,6 @@ import { MapFunction } from "../mapFunction";
  * ASTの構造としてはルートノードの下にこれらのノードが構文分だけぶら下がる形になります。
  */
 export type statementNode = DistanceNode | VarAssignNode | SyntaxNode
-
-/**
- * 全ての数式ASTノードです。
- * 引数や変数値はこのノードの内のどれかが入ります。
- */
-export type exprNode = AdditionNode | SubtractionNode | MultiplicationNode | DivisionNode | ModuloNode | UnaryNode
-  | AbsNode | Atan2Node | CeilNode | CosNode | ExpNode | FloorNode | LogNode | PowNode | RandNode | SinNode | SqrtNode
-  | NumberNode | DistanceVariableNode | StringNode | VarNode
 
 /**
  * マップ構文のASTノード種類
@@ -177,14 +170,6 @@ export abstract class SyntaxWithKeyNode extends SyntaxNode {
 }
 
 /**
- * 二項演算数式のASTノードベースクラス。
- */
-export abstract class InfixExpressionNode extends MapGrammarAstNode {
-  left: exprNode | null = null
-  right: exprNode | null = null
-}
-
-/**
  * ルートノード。
  * ASTの根となるノードで、範囲は全構文が含まれます。
  * 各構文はstatementsに格納されています。
@@ -212,175 +197,6 @@ export class VarAssignNode extends MapGrammarAstNode {
   varName: string | null = null
   value: exprNode | null = null
 }
-
-//#region 数式関係
-/**
- * 加算ノード。
- */
-export class AdditionNode extends InfixExpressionNode {
-  type: MapGrammarType = MapGrammarType.Calc_Addition
-}
-
-/**
- * 減算ノード。
- */
-export class SubtractionNode extends InfixExpressionNode {
-  type: MapGrammarType = MapGrammarType.Calc_Subtraction
-}
-
-/**
- * 乗算ノード。
- */
-export class MultiplicationNode extends InfixExpressionNode {
-  type: MapGrammarType = MapGrammarType.Calc_Multiplication
-}
-
-/**
- * 除算ノード。
- */
-export class DivisionNode extends InfixExpressionNode {
-  type: MapGrammarType = MapGrammarType.Calc_Division
-}
-
-/**
- * 剰余算ノード。
- */
-export class ModuloNode extends InfixExpressionNode {
-  type: MapGrammarType = MapGrammarType.Calc_Modulo
-}
-
-/**
- * ユーナリ演算ノード。
- */
-export class UnaryNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Unary
-  inner: exprNode | null = null
-}
-
-/**
- * 絶対値関数ノード。
- */
-export class AbsNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Abs
-  value: exprNode | null = null
-}
-
-/**
- * ArcTan関数ノード。
- */
-export class Atan2Node extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Atan2
-  y: exprNode | null = null
-  x: exprNode | null = null
-}
-
-/**
- * Ceil関数ノード。
- */
-export class CeilNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Ceil
-  value: exprNode | null = null
-}
-
-/**
- * Cos関数ノード。
- */
-export class CosNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Cos
-  value: exprNode | null = null
-}
-
-/**
- * Exp関数ノード。
- */
-export class ExpNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Exp
-  value: exprNode | null = null
-}
-
-/**
- * Floor関数ノード。
- */
-export class FloorNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Floor
-  value: exprNode | null = null
-}
-
-/**
- * Log関数ノード。
- */
-export class LogNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Log
-  value: exprNode | null = null
-}
-
-/**
- * Pow関数ノード。
- */
-export class PowNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Pow
-  x: exprNode | null = null
-  y: exprNode | null = null
-}
-
-/**
- * Rand関数ノード。
- */
-export class RandNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Rand
-  value: exprNode | null = null
-}
-
-/**
- * Sin関数ノード。
- */
-export class SinNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Sin
-  value: exprNode | null = null
-}
-
-/**
- * Sqrt関数ノード。
- */
-export class SqrtNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Calc_Sqrt
-  value: exprNode | null = null
-}
-//#endregion
-
-//#region 値
-
-/**
- * 数値ノード。
- */
-export class NumberNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Number
-  value: number | null = null
-}
-
-/**
- * 距離変数ノード。
- */
-export class DistanceVariableNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.DistanceVariable
-}
-
-/**
- * 文字列ノード。
- */
-export class StringNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.String
-  value: string | null = null
-}
-
-/**
- * 数値ノード。
- */
-export class VarNode extends MapGrammarAstNode {
-  type: MapGrammarType = MapGrammarType.Var
-  varName: string | null = null
-}
-//#endregion
 
 //#region Curve構文
 /**
